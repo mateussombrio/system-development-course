523789355c60b609c7dda36a140a0e9f496e8628
Mateus Sombrio <mateusombrio@gmail.com>
2025-02-21 12:05:26 -0300

    Organizing folders

--- course-exercises/highest-lowest.test.js
+++
@@ -1,9 +0,0 @@
-const highLow = require("./highest-lowest");
-
-test("Checks the highest and lowest values", () => {
-  const input = [3,6,2,8,5,10]
-  const expectedOutput = {
-    Highest : 10, Lowest: 2
-  }
-  expect(highLow(input)).toEqual(expectedOutput)
-});
