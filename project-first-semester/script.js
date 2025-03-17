15e3bb3743d67b7840592c80400b12e863bd7e84
Mateus Sombrio <mateusombrio@gmail.com>
2025-03-17 19:12:03 -0300

    Creating cards section

--- project-first-semester/script.js
+++ project-first-semester/script.js
@@ -3,14 +3,13 @@ let cardsData = [
   {
     title: "RTX 4070 Super",
     description: "Placa de vídeo",
-    image: "https://github.com/mateussombrio/system-development-course/blob/main/project-first-semester/assets/images/rtx-4070-super.png",
+    image: "../project-first-semester/assets/images/rtx-4070-super.png",
     price: "$ 350",
   },
   {
     title: 'LG UltraGear 24"',
     description: "Monitor LG",
-    image:
-      "https://dabee.com.br/wp-content/uploads/2024/11/Monitor-Gamer-LG-UltraGear-24-e-bom.webp",
+    image: "../project-first-semester/assets/images/monitor-lg-24.png",
     price: "$ 150",
   },
   {
@@ -23,8 +22,7 @@ let cardsData = [
   {
     title: "AMD Ryzen 7 5700X3D",
     description: "Processador Ryzen 7",
-    image:
-      "https://cdn.dooca.store/180/products/ryzen-5700x3d.png?v=1724437372&webp=0",
+    image: "../project-first-semester/assets/images/ryzen-5700x3d.png",
     price: "$ 100",
   },
   {
@@ -36,41 +34,40 @@ let cardsData = [
   {
     title: "Kingston Fury Beast, 16GB",
     description: "Memória RAM",
-    image:
-      "https://www.kingstonstore.com.br/cdn/shop/products/DDR4FBK11_9596bf21-5e69-487b-aa7c-4f1c6450cbd1.jpg?v=1627417974",
+    image: "../project-first-semester/assets/images/memoria-16gb.png",
     price: "$ 30",
   },
 ];
 
-
 // Creating Cards
-let div_cards = document.querySelector(".div_cards");
+const div_cards = document.querySelector(".div_cards");
 
 for (i = 0; i < cardsData.length; i++) {
   const cards = document.createElement("div");
   cards.classList.add("class_cards");
-// Div img and img element
-  const div_img_card = document.createElement('div')
-  div_img_card.classList.add('class_div_img_card')
+  // Div img and img element
+  const div_img_card = document.createElement("div");
+  div_img_card.classList.add("class_div_img_card");
   const img = document.createElement("img");
   img.classList.add("img_cards");
   img.src = cardsData[i].image;
   img.alt = cardsData[i].title;
-// H3 element
+  // H3 element
   const h3 = document.createElement("h3");
   h3.textContent = cardsData[i].title;
-// Price element
+  // Price element
   const price = document.createElement("p");
   price.textContent = cardsData[i].price;
-
+  // Buttom element
   const buttom_card = document.createElement("button");
+  buttom_card.classList.add("class_buttom");
   buttom_card.textContent = "Buy Now";
 
-  cards.appendChild(div_img_card)
-  div_img_card.appendChild(img)
+  cards.appendChild(div_img_card);
+  div_img_card.appendChild(img);
   cards.appendChild(h3);
   cards.appendChild(price);
-  cards.appendChild(buttom_card);   
+  cards.appendChild(buttom_card);
 
   div_cards.appendChild(cards);
 }
